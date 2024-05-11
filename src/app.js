c3b1cf844e538bbf745afb383ce007111eedab25
Cristian2499 <162387827+Cristian2499@users.noreply.github.com>
2024-05-11 16:19:23 +0000

    commit}

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".card").classList.add(generateRandomFigure());
+  document.querySelector(".number").innerHTML = generateRandomNumber();
+};
+
+let generateRandomFigure = () => {
+  let figures = ["spade", "club", "heart", "diamond"];
+  let randomFigure = Math.floor(Math.random() * figures.length);
+  return figures[randomFigure];
+};
+
+let generateRandomNumber = () => {
+  let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
+  let randomNumber = Math.floor(Math.random() * numbers.length);
+  return numbers[randomNumber];
 };
